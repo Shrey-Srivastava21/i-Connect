ec1fa571924540f2fe5aa4c1a24d68a0eab42edd
Shrey Srivastava <devshiva.1738@gmail.com>
2023-11-28 18:28:09 +0530

    commit my changes

--- client/src/utils/proxy.js
+++ client/src/utils/proxy.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const API = "https://i-connect-4tn4.onrender.com/api/v1";
-=======
-export const API = "https://i-connect-4tn4.onrender.com/api/v1"
->>>>>>> 0926573b0a4390c36e7d5ca5dd342cab532bf74b
